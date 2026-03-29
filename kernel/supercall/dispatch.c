7f63e984c19a403b6a95052a85d9501073d597df
bachnxuan <bachnxuan@gmail.com>
2026-03-29 22:24:18 +0700

    kernel, uapi: import our changes
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- kernel/supercall/dispatch.c
+++ kernel/supercall/dispatch.c
@@ -822,6 +822,53 @@ static int do_get_hook_type(void __user *arg)
     return 0;
 }
 
+#ifdef CONFIG_KSU_MULTI_MANAGER_SUPPORT
+static int do_get_version_tag(void __user *arg)
+{
+    struct ksu_get_version_tag_cmd cmd = { 0 };
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
+    strscpy(cmd.tag, KSU_VERSION_FULL, sizeof(cmd.tag));
+#else
+    strlcpy(cmd.tag, KSU_VERSION_FULL, sizeof(cmd.tag));
+#endif
+
+    if (copy_to_user(arg, &cmd, sizeof(cmd))) {
+        pr_err("get_version_tag: copy_to_user failed\n");
+        return -EFAULT;
+    }
+
+    return 0;
+}
+
+static int do_get_hook_mode(void __user *arg)
+{
+    struct ksu_get_hook_mode_cmd cmd = { 0 };
+#if defined(KSU_TP_HOOK)
+    const char *type = "Tracepoint Syscall Redirect";
+#elif defined(CONFIG_KSU_MANUAL_HOOK)
+    const char *type = "Manual";
+#elif defined(CONFIG_KSU_SUSFS)
+    const char *type = "Inline";
+#else
+#error "Unsupported hook type"
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
+    strscpy(cmd.mode, type, sizeof(cmd.mode));
+#else
+    strlcpy(cmd.mode, type, sizeof(cmd.mode));
+#endif
+
+    if (copy_to_user(arg, &cmd, sizeof(cmd))) {
+        pr_err("get_hook_mode: copy_to_user failed\n");
+        return -EFAULT;
+    }
+
+    return 0;
+}
+#endif
+
 // 102. ENABLE_KPM - Check if KPM is enabled
 static int do_enable_kpm(void __user *arg)
 {
@@ -1039,6 +1086,16 @@ static const struct ksu_ioctl_cmd_map ksu_ioctl_handlers[] = {
         .handler = do_enable_kpm, 
         .perm_check = manager_or_root 
     },
+#ifdef CONFIG_KSU_MULTI_MANAGER_SUPPORT
+    { .cmd = KSU_IOCTL_GET_HOOK_MODE,
+      .name = "GET_HOOK_MODE",
+      .handler = do_get_hook_mode,
+      .perm_check = manager_or_root },
+	{ .cmd = KSU_IOCTL_GET_VERSION_TAG,
+	  .name = "GET_VERSION_TAG",
+	  .handler = do_get_version_tag,
+	  .perm_check = manager_or_root },
+#endif
     { 
         .cmd = KSU_IOCTL_DYNAMIC_MANAGER,
         .name = "SET_DYNAMIC_MANAGER",
